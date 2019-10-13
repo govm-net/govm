66db60cb37c8239793b8ee1c8e764dbdd28720b7
lengzhao <zgq_126@126.com>
2019-10-13 17:36:58 +0800

    add setting.html

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git pull
-go build -v -ldflags "-X github.com/lengzhao/govm/conf.BuildTime=$(date +%Y-%m%d-%H:%M) -X github.com/lengzhao/govm/conf.GitHead=$(git rev-parse HEAD | echo unknow)"
+go build -v -ldflags "-X github.com/lengzhao/govm/conf.BuildTime=$(date +%Y-%m%d-%H:%M) -X github.com/lengzhao/govm/conf.GitHead=$(git rev-parse HEAD)"
 
 while true
 do
