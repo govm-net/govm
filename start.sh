213ec988142d3648f92b068d2f3534ef74f734f1
lengzhao <zgq_126@126.com>
2019-10-13 17:47:07 +0800

    not do git pull when start

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-git pull
 go build -v -ldflags "-X github.com/lengzhao/govm/conf.BuildTime=$(date +%Y-%m%d-%H:%M) -X github.com/lengzhao/govm/conf.GitHead=$(git rev-parse HEAD)"
 
 while true
 do
-    echo start govm
+    echo start govm. you can use "Ctrl + c" to exit
     date
     ./govm
     if [ "$?" -eq "127" ]
