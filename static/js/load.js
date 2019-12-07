8b1ad8902b4deb452e7c3af906c0dd083845566e
lengzhao <zgq_126@126.com>
2019-12-07 18:57:59 +0800

    need reload navbar.page

--- static/js/load.js
+++ static/js/load.js
@@ -13,7 +13,7 @@ if (gCostBase == "") {
 }
 gLanguage = getCookie("language")
 
-$.get("navbar.page", function (data) {
+$.get("navbar.page?v=2", function (data) {
     $("#navbar").html(data);
     var url = window.location.pathname;
     if (url == "/") {
