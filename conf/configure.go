a29bf9a6284416d369cdf1adeed6d1f10a310f0e
lengzhao <zgq_126@126.com>
2019-12-10 20:37:04 +0800

    update version

--- conf/configure.go
+++ conf/configure.go
@@ -39,7 +39,7 @@ const (
 var (
 	conf TConfig
 	// Version software version
-	Version string = "v0.3.4"
+	Version string = "v0.3.5"
 	// BuildTime build time
 	BuildTime string
 	// GitHead git head
